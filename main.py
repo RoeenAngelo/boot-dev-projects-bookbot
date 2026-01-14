57b42c2b9b71dadf094a76277ad74db193115294
Roeen Acal <roeenacal@Ros-MacBook-Air.local>
2026-01-15 02:02:11 +0800

    Configure book_path

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from stats import get_num_words, get_num_letters
+from stats import get_num_words, get_num_letters, chars_dict_to_sorted_list, sys
+
+
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if sys.argv.__len__() != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_num_letters(text)
-    print(f"Found {num_words} total words")
-    print(char_count)
+    sorted_char_count_list = chars_dict_to_sorted_list(char_count)
+    print_report(book_path, num_words, sorted_char_count_list)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
+
+def print_report(book_path, num_words, sorted_char_count_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("-------- Character Count --------")
+    for item in sorted_char_count_list:
+        char = item["char"]
+        count = item["num"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -19,3 +21,14 @@ def get_num_letters(text):
         else:
             letter_counts[char] = 1
     return letter_counts
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list

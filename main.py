fa51d4901bd39be3394edba66c9fb034bd5acd6b
Roeen Acal <roeenacal@Ros-MacBook-Air.local>
2026-01-14 23:58:11 +0800

    Add get_num_letters function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_letters
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_count = get_num_letters(text)
     print(f"Found {num_words} total words")
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+# convert text into a list.
+# loop through the list and count letter
+# make sure to include spaces and symbols
+# all letters should be lowercase
+#return a dictionary with letter as key and count as value
+
+def get_num_letters(text):
+    text = text.lower()
+    letter_counts = {}
+    text_list = list(text)
+
+    for char in text_list:
+        if char in letter_counts:
+            letter_counts[char] += 1
+        else:
+            letter_counts[char] = 1
+    return letter_counts

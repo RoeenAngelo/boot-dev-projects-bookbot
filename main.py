37878db0eea5fe1b48526f211d2b94173d8f12f3
Roeen Acal <roeenacal@Ros-MacBook-Air.local>
2026-01-14 23:33:15 +0800

    Refactor and create stats.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from stats import get_num_words
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
